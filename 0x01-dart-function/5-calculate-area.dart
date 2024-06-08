0b3c647c364d9bb621ccf6a657d2e582587f3c58
Nomad-Rob <rfarley89@gmail.com>
2024-06-08 15:58:29 -0500

    Task 5 complete

---
+++ 0x01-dart-function/5-calculate-area.dart
@@ -0,0 +1,5 @@
+// Function to calculate the area of a triangle and round the result to two decimal places.
+double calculateArea(double height, double base) {
+  double area = 0.5 * base * height;
+  return double.parse(area.toStringAsFixed(2));
+}

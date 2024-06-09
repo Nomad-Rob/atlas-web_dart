cc497c59ba8bbaaf81e6cbfedcdda9298e3ad784
Nomad-Rob <rfarley89@gmail.com>
2024-06-08 23:15:34 -0500

    Try again

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -7,16 +7,14 @@ class User extends Password {
   int age;
   double height;
 
-  // User constructor
   User({
     required this.id,
     required this.name,
     required this.age,
     required this.height,
-    String? user_password,
-  }) : super(password: user_password);
+    String? password,
+  }) : super(password: password);  // Pass password to the superclass constructor
 
-  // Method to convert a user object to a json object
   Map<String, dynamic> toJson() {
     return {
       'id': id,
@@ -26,18 +24,16 @@ class User extends Password {
     };
   }
   
-  // Method to convert a json object to a user object
   static User fromJson(Map<dynamic, dynamic> userJson) {
     return User(
       id: userJson['id'] as int,
       name: userJson['name'] as String,
       age: userJson['age'] as int,
       height: userJson['height'] as double,
-      user_password: userJson['user_password'] as String,
+      password: userJson['user_password'] as String,
     );
   }
 
-  // Override is how we override a method from the parent class
   @override
   String toString() {
     return 'User(id : $id ,name: $name, age: $age, height: $height, Password: ${isValid()})';

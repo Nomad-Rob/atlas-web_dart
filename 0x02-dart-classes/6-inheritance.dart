4087796c0e45e6f19a5bc0cd9d77d6600ba1098b
Nomad-Rob <rfarley89@gmail.com>
2024-06-08 23:19:52 -0500

    trying again ..

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -1,6 +1,6 @@
 import '6-password.dart';
 
-// class User extends Password {
+// class User extends Password {import '6-password.dart';
 class User extends Password {
   int id;
   String name;
@@ -12,30 +12,30 @@ class User extends Password {
     required this.name,
     required this.age,
     required this.height,
-    String? password,
-  }) : super(password: password);  // Pass password to the superclass constructor
+    String? user_password,
+  }) : super(password: user_password);
 
   Map<String, dynamic> toJson() {
     return {
       'id': id,
       'name': name,
       'age': age,
-      'height': height,
+      'height': height
     };
   }
-  
+
   static User fromJson(Map<dynamic, dynamic> userJson) {
     return User(
       id: userJson['id'] as int,
       name: userJson['name'] as String,
       age: userJson['age'] as int,
       height: userJson['height'] as double,
-      password: userJson['user_password'] as String,
+      user_password: userJson['user_password'] as String,
     );
   }
 
   @override
   String toString() {
-    return 'User(id : $id ,name: $name, age: $age, height: $height, Password: ${isValid()})';
+    return 'User(id : $id, name: $name, age: $age, height: $height, Password: ${isValid()})';
   }
 }
--- 0x02-dart-classes/6-main.dart
+++ 0x02-dart-classes/6-main.dart
@@ -6,6 +6,7 @@ void main() {
       id: 1, name: "Djo", age: 25, height: 1.89, user_password: "Azert23defde");
   print('<===========Json=========>:\n');
   print(djo.toJson());
+
   Map map = {
     'id': 3,
     'name': 'Youssef',
@@ -14,9 +15,11 @@ void main() {
     'user_password': "Azert23defde"
   };
   final youssef = User.fromJson(map);
+
   print('\n<===========Test1===========>:\n');
   print(djo.toString());
   print(youssef.toString());
+
   print('\n<===========Test2===========>:\n');
   djo.password = "holberton98";
   youssef.password = "AZERfghn6789";

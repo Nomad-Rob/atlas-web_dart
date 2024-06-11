f1c1b9c2217fc16fc504b85e6e0fede63a7201e4
Nomad-Rob <rfarley89@gmail.com>
2024-06-10 19:43:02 -0500

    Change on the function and removed accidently code for 1-util.dart

--- 0x03-asynchronous/1-get_user_id.dart
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -1,14 +1,9 @@
-import 'dart:convert';
-
-// Function that fetches user data from the server
-Future<String> fetchUserData() => Future.delayed(
-      const Duration(seconds: 2),
-      () => '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
-    );
+import '1-util.dart';
+import 'dart:convert' as convert;
 
 // Function that returns the user id
 Future<String> getUserId() async {
-  String jsonData = await fetchUserData();
-  var decodedData = jsonDecode(jsonData);
-  return decodedData['id'];
+  var userData = await fetchUserData();
+  Map<String, dynamic> data = convert.jsonDecode(userData);
+  return data['id'];
 }

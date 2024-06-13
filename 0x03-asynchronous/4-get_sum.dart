851c0c8f83f2a173da89c15133fed951fe15f992
Nomad-Rob <rfarley89@gmail.com>
2024-06-12 20:23:44 -0500

    Switching from string to a var

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -5,13 +5,13 @@ Future<String> calculateTotal() async {
   double totalPrice = 0.0;
   try {
     // Get User data - id
-    String userData = await fetchUserData();
+    var userData = await fetchUserData();
     Map<String, dynamic> user = convert.jsonDecode(userData);
-    String userId = user['id'];
+    var userId = user['id'];
 
     // Get user orders
-    String userOrdersData = await fetchUserOrders(userId);
-    List<dynamic> orders = convert.jsonDecode(userOrdersData);
+    var userOrders = await fetchUserOrders(userId);
+    List<dynamic> orders = convert.jsonDecode(userOrders);
 
     // Iterate over each item in the orders and get its price
     for (var item in orders) {
@@ -22,7 +22,6 @@ Future<String> calculateTotal() async {
 
     return totalPrice.toString();
   } catch (e) {
-    print('Error caught: $e');
     return '-1';
   }
 }
